18bffab3b495949ccfbe5741452b5d5bed9e9aeb
Symbolics <steve@symbolics.tech>
2021-09-12 09:43:59 +0800

    Add ability to read from streams and strings, make work on Genera

--- importers.lisp
+++ importers.lisp
@@ -1,27 +1,55 @@
-;;;; Simple table data structure.
-;;;; Copyright (c) 2013, Francisco Soto All rights reserved (see COPYING file for details).
-
+;;; -*- Mode: LISP; Base: 10; Syntax: ANSI-Common-Lisp; Package: CL-SIMPLE-TABLE -*-
 (in-package :cl-simple-table)
 
-(defun table-from-file (filename &key (separator #\tab) parse-elements)
-  "Reads the tabular data file and returns the contents. Separator is TAB by default.
+(defparameter *default-external-format* :default
+  "External format used for opening files")
+
+(defun white-space? (c)
+  (member c '(#\newline #\tab #\space #\return)))
+
+(defvar *quote* #\" "Default quote character")
+(defvar *separator* #\, "Default separator character")
+(defvar *escape* #\" "Default escape character")
+
+;;; I/O from various sources
+(defun %in-stream (stream-or-string)
+  (typecase stream-or-string
+    (string (make-string-input-stream stream-or-string))
+    (stream stream-or-string)
+    (pathname (values (open stream-or-string :external-format *default-external-format*)
+                      T))))
+
+(defmacro with-csv-input-stream ((name inp) &body body)
+  (alexandria:with-unique-names (opened?)
+    `(multiple-value-bind (,name ,opened?) (%in-stream ,inp)
+      (flet ((body () ,@body))
+        (unwind-protect (body)
+          (when (and ,name ,opened?)
+            (close ,name)))))))
+
+
+;;; Readers
+(defun table-from-stream (stream-or-string &key (separator *separator*) parse-elements skip-first-row?)
+  "Reads the tabular data file from a string, stream or filepath and returns the contents. Separator is TAB by default.
    If parse-elements is other than NIL elements from the table will be READ into Lisp objects,
    otherwise only strings will be created."
-  (let ((filter (if parse-elements
-                    (lambda (ln) (mapcar (lambda (el) (read-from-string el nil))
-                                         (split-string separator ln)))
-                    (lambda (ln) (split-string separator ln)))))
-    (with-open-file (s filename :if-does-not-exist nil)
+  (let* ((filter (if parse-elements
+                     (lambda (ln) (mapcar (lambda (el) (read-from-string el nil))
+				     (split-string separator ln)))
+		     (lambda (ln) (split-string separator ln)))))
+    (with-csv-input-stream (s stream-or-string)
       (row-sequence->table
        (loop
           for line = (read-line s nil nil)
           until (null line)
-          collect (sequence->row (funcall filter line)))))))
+	  unless skip-first-row?
+	    collect (sequence->row (funcall filter line))
+	  do (setf skip-first-row? nil))))))
 
-(defun read-csv (filename &optional parse-elements)
+(defun read-csv (filename &key parse-elements skip-first-row?)
   "Creates a table from a comma-separated values file."
-  (table-from-file filename :separator #\, :parse-elements parse-elements))
+  (table-from-stream filename :separator #\, :parse-elements parse-elements :skip-first-row? skip-first-row?))
 
-(defun read-tsv (filename &optional parse-elements)
+(defun read-tsv (filename &key parse-elements skip-first-row?)
   "Creates a table from a tab-separated values file."
-  (table-from-file filename :separator #\tab :parse-elements parse-elements))
+  (table-from-stream filename :separator #\tab :parse-elements parse-elements :skip-first-row? skip-first-row?))
